5b0b7c93f39008a7ba6e4a5730567800593f044f
epidemicflu <hcedillo2018@bklawtech.com>
2016-07-06 19:37:00 +0000

    Working on the api

---
+++ js/index.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+	// this is the code that runs when the
+	// screen loads
+
+};
+
+/*function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}*/
+
+function exampleFunction() {
+	// this is an example function with no parameters
+
+}
